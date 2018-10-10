e1f8443da4cb80eafc80f041bf8cce1a42cc17da
Matthew Slipper <me@matthewslipper.com>
2018-10-10 02:23:06 -0700

    Add socket transport

--- node/node.go
+++ node/node.go
@@ -570,7 +570,7 @@ func (n *Node) OpenDatabase(name string, cache, handles int) (ethdb.Database, er
 	if n.config.DataDir == "" {
 		return ethdb.NewMemDatabase(), nil
 	}
-	return ethdb.NewLevelSRVDatabase("localhost:5900")
+	return ethdb.NewLevelSRVDatabase("unix:///tmp/levelsrv.sock")
 }
 
 // ResolvePath returns the absolute path of a resource in the instance directory.
--- node/service.go
+++ node/service.go
@@ -43,7 +43,7 @@ func (ctx *ServiceContext) OpenDatabase(name string, cache int, handles int) (et
 	if ctx.config.DataDir == "" {
 		return ethdb.NewMemDatabase(), nil
 	}
-	db, err := ethdb.NewLevelSRVDatabase("localhost:5900")
+	db, err := ethdb.NewLevelSRVDatabase("unix:///tmp/levelsrv.sock")
 	if err != nil {
 		return nil, err
 	}
